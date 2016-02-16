8b1965982ac846614f87e1f576843be2dacfd51b
rain_home <liu044100@gmail.com>
2016-02-16 23:44:31 +0900

    Initial Commit

---
+++ SmileRoundedTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileRoundedTable
+//
+//  Created by yuchen liu on 2/16/16.
+//  Copyright © 2016 rain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
