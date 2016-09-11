de26783c38ad59bcb2fd27a3d0687e16bfe8634c
rain_home <liu044100@gmail.com>
2016-09-11 21:01:24 +0900

    mod. rounded logic

--- SmileRoundedTable/SmileRoundedTableViewCell.swift
+++ SmileRoundedTable/SmileRoundedTableViewCell.swift
@@ -7,15 +7,79 @@
 
 import UIKit
 
+@IBDesignable
+public class SmileRoundedTableView: UITableView {
+    @IBInspectable public var margin: CGFloat = 28
+    @IBInspectable public var cornerRadius: CGFloat = 8
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCellsMargin()
+        configureCellsCornerRadius()
+        configureHeaderFooterMargin()
+    }
+    
+    private func configureCellsCornerRadius() {
+        roundedTableViewCells()?
+            .forEach {
+                $0.cornerRadius = cornerRadius
+        }
+    }
+    
+    private func configureCellsMargin() {
+        roundedTableViewCells()?
+            .filter {
+                $0.frame.size.width + 2 * margin != self.frame.size.width
+            }
+            .forEach { cell in
+                cell.frame.origin.x += margin
+                cell.frame.size.width -= 2 * margin
+        }
+    }
+    
+    private func roundedTableViewCells() -> [SmileRoundedTableViewCell]? {
+        let wrapperView = self.subviews.first
+        return wrapperView?.subviews.filter { $0 is SmileRoundedTableViewCell } as? [SmileRoundedTableViewCell]
+    }
+    
+    private func configureHeaderFooterMargin() {
+        tableHeaderFooterViews()
+            .filter {
+                $0.frame.origin.x != margin
+            }
+            .forEach {
+                $0.frame.origin.x = margin
+        }
+    }
+    
+    private func tableHeaderFooterViews() -> [UIView] {
+        let headerViews = (0..<numberOfSections)
+            .map { headerViewForSection($0) }
+            .flatMap { $0 }
+        
+        let footerViews = (0..<numberOfSections)
+            .map { footerViewForSection($0) }
+            .flatMap { $0 }
+        
+        return headerViews + footerViews
+    }
+}
 
-///This class imitate the default separator & selectionStyle, the tableViewCell handle the style of self
+///This class imitate the default selectionStyle, the tableViewCell handle the style of self
 @IBDesignable
 public class SmileRoundedTableViewCell: UITableViewCell {
-
-    //MARK: IBInspectable Public Property
-    @IBInspectable public var margin: CGFloat = 28
-    @IBInspectable public var cornerRadius: CGFloat = 6
     
+    //MARK: Public Property
+    public var cornerRadius: CGFloat = 0 {
+        didSet {
+            guard oldValue != cornerRadius else { return }
+            roundedView.layer.cornerRadius = cornerRadius
+            configureContentViewFrame()
+        }
+    }
+    
+    //MARK: IBInspectable Public Property
     @IBInspectable public var selectedColor: UIColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
     @IBInspectable public var contentColor: UIColor = UIColor.whiteColor() {
         didSet {
@@ -23,33 +87,15 @@ public class SmileRoundedTableViewCell: UITableViewCell {
         }
     }
     
-    //MARK: Constant
-    private let defaultSeparatorColor = UIColor(red: 206/255, green: 206/255, blue: 210/255, alpha: 1)
-    
     //MARK: Private Property
     ///the view cover top corner of contentView
     private let topCornerCoverView = UIView()
-    
     ///the view cover bottom corner of contentView
     private let bottomCornerCoverView = UIView()
-    
     private let roundedView = UIView()
-    
-    private let separatorView = UIView()
     
     private var contentViews: [UIView] {
         return [roundedView, topCornerCoverView, bottomCornerCoverView]
-    }
-    
-    //MARK: Setter
-    override public var frame: CGRect {
-        didSet {
-            super.frame.origin.x += margin
-            guard let tableview = tableview() else { return }
-            //only change frame when cell frame == talbe view frame
-            guard tableview.frame.width == super.frame.size.width else { return }
-            super.frame.size.width -= 2 * margin
-        }
     }
     
     //MARK: Selected
@@ -64,17 +110,10 @@ public class SmileRoundedTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
         handleSelected(selected, animated: animated)
     }
-
+    
     //MARK: Life Cycle
     public override func didMoveToSuperview() {
         super.didMoveToSuperview()
-        
-        guard let tableView = tableview() else { return }
-        
-        self.separatorView.backgroundColor = tableView.separatorColor ?? defaultSeparatorColor
-        
-        //Because this class create a new separator view to imitate the default separator, so set tableView separatorStyle to None
-        tableView.separatorStyle = .None
         
         //Because this class imitate the default selection, so disable default selectionStyle
         self.selectionStyle = .None
@@ -84,42 +123,26 @@ public class SmileRoundedTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
         //color
-        self.selectedBackgroundView = nil
         self.backgroundColor        = UIColor.clearColor()
         contentView.backgroundColor = UIColor.clearColor()
         
-        //corner radius
-        roundedView.layer.cornerRadius = cornerRadius
-
         //add subview
         self.insertSubview(roundedView, belowSubview: contentView)
         self.insertSubview(topCornerCoverView, belowSubview: roundedView)
         self.insertSubview(bottomCornerCoverView, belowSubview: contentView)
-        bottomCornerCoverView.addSubview(separatorView)
         
         //set frame
-        let width  = self.bounds.size.width
-        let height = self.bounds.size.height
-        let origin = self.bounds.origin
-        let coverViewSize = CGSize(width: width, height: height - cornerRadius)
-        let separatorHeight: CGFloat = 0.5
-        
-        roundedView.frame   = self.bounds
-        separatorView.frame = CGRect(origin: CGPoint(x: 0, y: coverViewSize.height - separatorHeight), size: CGSize(width: width, height: separatorHeight))
-        topCornerCoverView.frame    = CGRect(origin: origin, size: coverViewSize)
-        bottomCornerCoverView.frame = CGRect(origin: CGPoint(x: origin.x, y: origin.y + cornerRadius), size: coverViewSize)
+        configureContentViewFrame()
         
         //autoresize
-        roundedView.autoresizingMask   = [.FlexibleWidth, .FlexibleHeight]
-        separatorView.autoresizingMask = [.FlexibleWidth, .FlexibleLeftMargin, .FlexibleRightMargin]
-        topCornerCoverView.autoresizingMask    = [.FlexibleTopMargin, .FlexibleWidth, .FlexibleHeight]
+        roundedView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        topCornerCoverView.autoresizingMask = [.FlexibleTopMargin, .FlexibleWidth, .FlexibleHeight]
         bottomCornerCoverView.autoresizingMask = [.FlexibleBottomMargin, .FlexibleWidth, .FlexibleHeight]
     }
-
+    
     public override func layoutSubviews() {
         super.layoutSubviews()
-        updateCorner()
-        updateSeparator()
+        updateRoundCorner()
     }
     
     //MARK: Help Method
@@ -130,31 +153,51 @@ public class SmileRoundedTableViewCell: UITableViewCell {
         return view
     }
     
-    private func updateCorner() {
+    private func configureContentViewFrame() {
+        let width  = self.bounds.size.width
+        let height = self.bounds.size.height
+        let origin = self.bounds.origin
+        let coverViewSize = CGSize(width: width, height: height - cornerRadius)
+        
+        roundedView.frame = self.bounds
+        topCornerCoverView.frame = CGRect(origin: origin, size: coverViewSize)
+        bottomCornerCoverView.frame = CGRect(origin: CGPoint(x: origin.x, y: origin.y + cornerRadius), size: coverViewSize)
+    }
+    
+    private func updateRoundCorner() {
         //update cell round corner style
         guard let tableView = tableview(), indexPath = tableView.indexPathForRowAtPoint(self.center) else { return }
         
         if indexPath.row == 0 && tableView.numberOfRowsInSection(indexPath.section) == 1 {
             self.topCornerCoverView.hidden    = true
             self.bottomCornerCoverView.hidden = true
+            hideSeparator(true)
         } else if indexPath.row == 0 {
             self.topCornerCoverView.hidden    = true
             self.bottomCornerCoverView.hidden = false
+            hideSeparator(false)
         } else if indexPath.row == tableView.numberOfRowsInSection(indexPath.section) - 1 {
             self.topCornerCoverView.hidden    = false
             self.bottomCornerCoverView.hidden = true
+            hideSeparator(true)
         } else {
             self.topCornerCoverView.hidden    = false
             self.bottomCornerCoverView.hidden = false
+            hideSeparator(false)
         }
     }
     
-    private func updateSeparator() {
-        let leftInset  = self.separatorInset.left
+    private func hideSeparator(hidden: Bool) {
+        let separatorViews = self.subviews.filter {
+            //separator line height < 1
+            $0.frame.height < 1
+        }
         
-        let previousOrigin = separatorView.frame.origin
+        //always hide cell's top separator
+        separatorViews.filter { $0.frame.origin.y == 0 }.first?.hidden = true
         
-        separatorView.frame.origin = CGPoint(x: leftInset, y: previousOrigin.y)
+        //configure bottom separator
+        separatorViews.filter { $0.frame.origin.y != 0 }.first?.hidden = hidden
     }
     
     private func handleSelected(highlighted: Bool, animated: Bool) {
